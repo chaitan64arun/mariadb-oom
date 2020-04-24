11418a2a6a4078173b906e6c4154f471986641e1
Arun Chaitanya Miriappalli <chaitan64arun@gmail.com>
2020-04-24 11:42:41 +0800

    My initial commit

---
+++ src/main/java/com/github/chaitan64arun/mariadb/oom/App.java
@@ -0,0 +1,12 @@
+package com.github.chaitan64arun.mariadb.oom;
+
+/**
+ * Simple main application
+ */
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
